d33e4fc3de1afdc68de84231ecfbe9482f57cb41
Anurag Gharat <53979540+anurag-gharat@users.noreply.github.com>
2020-07-20 00:16:43 +0530

    add post is done

--- src/app/pages/addpost/addpost.component.ts
+++ src/app/pages/addpost/addpost.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+import {NgForm} from '@angular/forms'
+import {finalize} from 'rxjs/operators'
+import { AngularFireStorage } from "@angular/fire/storage";
+import {AngularFireDatabase} from '@angular/fire/database'
+import { readAndCompressImage } from 'browser-image-resizer';
+import {imageconfig} from '../../../Utils/config'
+import {v4 as uuidv4} from 'uuid'
 
 @Component({
   selector: 'app-addpost',
@@ -7,9 +17,80 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddpostComponent implements OnInit {
 
-  constructor() { }
+
+  locationName:string;
+  description:string;
+  picture:string=null
+  user=null
+  uploadPercent:number=null
+
+
+  constructor(
+    private toastr:ToastrService,
+    private auth:AuthService,
+    private db:AngularFireDatabase,
+    private storage:AngularFireStorage,
+    private router : Router
+  ) { 
+
+    auth.getUser().subscribe((user)=>{
+      console.log(user)
+      this.db.object(`/users/${user.uid}`)
+      .valueChanges()
+      .subscribe((user)=>{
+        this.user=user
+      })
+    })
+  }
+
+
+
 
   ngOnInit(): void {
+  }
+  onSubmit(){
+    const uid=uuidv4()
+    this.db.object(`/posts/${uid}`)
+    .set({
+      id:uid,
+      locationName:this.locationName,
+      description:this.description,
+      picture:this.picture,
+      by:this.user.name,
+      instaId:this.user.instaUserName,
+      date:Date.now()
+    })
+    .then(()=>{
+      this.toastr.success("Post added Successfully")
+      this.router.navigateByUrl('/')
+    })
+    .catch((err)=>{
+      this.toastr.error(err.message)
+    })
+  }
+
+  async uploadFile(event){
+
+    const file = event.target.files[0]
+    let resizedImage= await readAndCompressImage(file,imageconfig)
+    const filePath=file.name
+    const fileRef=this.storage.ref(filePath)
+
+
+    const task = this.storage.upload(filePath,resizedImage)
+    task.percentageChanges().subscribe((percentage)=>{
+        this.uploadPercent=percentage
+    })
+
+    task.snapshotChanges().pipe(
+      finalize(()=>{
+        fileRef.getDownloadURL().subscribe(url=>{
+          this.picture=url
+          this.toastr.success("Image Upload Success")
+        })
+      })
+    ).subscribe()
+
   }
 
 }

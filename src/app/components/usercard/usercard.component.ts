f26ad5c35d55a99032fde29161fd8e6805732067
Anurag Gharat <53979540+anurag-gharat@users.noreply.github.com>
2020-07-20 15:25:03 +0530

    users component done

--- src/app/components/usercard/usercard.component.ts
+++ src/app/components/usercard/usercard.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import {AuthService} from '../../services/auth.service'
-import {ToastrService} from 'ngx-toastr'
-import { Router } from '@angular/router';
 import { AngularFireDatabase } from '@angular/fire/database';
 
 
